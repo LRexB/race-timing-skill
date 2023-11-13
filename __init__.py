4640dbfba14711a63980bc6f0915c3c2848cd8a6
Rex Benning <rbenning@emergencesc.com>
2023-11-13 18:39:54 -0500

    initial setup, getting connection to Firebase

--- __init__.py
+++ __init__.py
@@ -1,13 +1,24 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import firebase_admin
+from firebase_admin import firestore
 
 class RaceTiming(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        app = firebase_admin.initialize_app()
+        self.db = firestore.client()
+
 
     @intent_file_handler('timing.race.intent')
     def handle_timing_race(self, message):
-        self.speak_dialog('timing.race')
+        races_ref = self.db.collection('races')
+        races = races_ref.stream()
+        self.log.info("got races")
+        for race in races :
+            self.log.info(f"{race.id} => {race.to_dict()}")
+            bib = race.to_dict()['1']['bibNumber']
+            time = race.to_dict()['1']['chipTime']
+            self.speak_dialog('timing.race', data= {'bib1':bib, 'time1' : time})
 
 
 def create_skill():
