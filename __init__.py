4aa39ca7a37ca30b24dafaba214ff70837e4600b
Rex Benning <rbenning@emergencesc.com>
2023-11-15 19:16:39 -0500

    refining firebase accesses

--- __init__.py
+++ __init__.py
@@ -12,12 +12,15 @@ class RaceTiming(MycroftSkill):
     @intent_file_handler('timing.race.intent')
     def handle_timing_race(self, message):
         races_ref = self.db.collection('races')
-        races = races_ref.stream()
+        race_id =  '2023-10-27T15:37:30.519419 - finish, lap 1'
+        race_result_doc = races_ref.document(race_id)
+        race_result_data = race_result_doc.get().to_dict()
         self.log.info("got races")
-        for race in races :
-            self.log.info(f"{race.id} => {race.to_dict()}")
-            bib = race.to_dict()['1']['bibNumber']
-            time = race.to_dict()['1']['chipTime']
+        self.log.info(race_result_data)
+        for bib_number in race_result_data.keys() : #{'1', '2', '7'} :
+            bib = race_result_data[bib_number]['bibNumber']
+            time = race_result_data[bib_number]['chipTime']
+            self.log.info(f"{bib_number} => {bib} => {time}")
             self.speak_dialog('timing.race', data= {'bib1':bib, 'time1' : time})
 
 
