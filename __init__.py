ce4a797e9574d15460149766de2913bbf1f70f18
Rex Benning <rbenning@emergencesc.com>
2023-11-16 21:44:53 -0500

    fixups

--- __init__.py
+++ __init__.py
@@ -8,45 +8,43 @@ class RaceTiming(MycroftSkill):
         app = firebase_admin.initialize_app()
         self.db = firestore.client()
 
-
     @intent_file_handler('timing.race.intent')
     def handle_timing_race(self, message):
-        races_ref = self.db.collection('races')
+        racesRef = self.db.collection('races')
         race_id =  'race start'
-        race_result_doc = races_ref.document(race_id)
-        race_result_data = race_result_doc.get().to_dict()
+        raceResultDoc = racesRef.document(race_id)
+        raceResultData = raceResultDoc.get().to_dict()
         self.log.info("got races")
-        self.log.info(race_result_data)
-        time_list = {}
-        lap_results = {}
+        self.log.info(raceResultData)
+        timeList = {}
+        lapResults = {}
 
-        for lap_number in race_result_data.keys() :
-            lap_results = race_result_data[lap_number]
-            if lap_number == '0' :
+        for lapNumber in sorted(raceResultData.keys()) :
+            lapResults = raceResultData[lapNumber]
+            if lapNumber == '0' :
                 self.log.info("setting start")
-                for bib_number in lap_results.keys() : 
-                    bib_list = {}
-                    bib_list['start_tick'] = bib_list['last_lap_time'] = lap_results[bib_number]['currentTick']
-                    bib = lap_results[bib_number]['bibNumber']
-                    bib_list['start_time'] = lap_results[bib_number]['startTime']
-                    time_list[bib_number] = bib_list
-                    self.log.info(f"{lap_number} = >{bib_number} => {bib} => {time_list[bib_number]}")
+                for bibNumber in lapResults.keys() : 
+                    bibList = {}
+                    bibList['startTick'] = bibList['lastLapTime'] = lapResults[bibNumber]['currentTick']
+                    bib = lapResults[bibNumber]['bibNumber']
+                    bibList['startTime'] = lapResults[bibNumber]['startTime']
+                    timeList[bibNumber] = bibList
+                    self.log.info(f"{lapNumber} = >{bibNumber} => {bib} => {timeList[bibNumber]}")
             else :
-                self.log.info(f"setting lap {lap_number}")
-                for bib_number in lap_results.keys() :
-                    bib = lap_results[bib_number]['bibNumber']
-                    time = (lap_results[bib_number]['currentTick'] - time_list[bib_number]['last_lap_time'])/1000
-                    time_list[bib_number]['last_lap_time'] = lap_results[bib_number]['currentTick']
-                    self.log.info(f"{lap_number} = >{bib_number} => {bib} => {time}")
-                    self.speak_dialog('timing.race', data= {'lap1': lap_number, 'bib1':bib, 'time1' : time})
+                self.log.info(f"setting lap {lapNumber}")
+                for bibNumber in lapResults.keys() :
+                    bib = lapResults[bibNumber]['bibNumber']
+                    time = (lapResults[bibNumber]['currentTick'] - timeList[bibNumber]['lastLapTime'])/1000
+                    timeList[bibNumber]['lastLapTime'] = lapResults[bibNumber]['currentTick']
+                    self.log.info(f"{lapNumber} = >{bibNumber} => {bib} => {time}")
+                    self.speak_dialog('timing.race', data= {'lap1': lapNumber, 'bib1':bib, 'time1' : time})
 
         self.log.info("end race")
-        for bib_number in lap_results.keys() : 
-            bib = lap_results[bib_number]['bibNumber']
-            time = (time_list[bib_number]['last_lap_time'] - time_list[bib_number]['start_tick'])/1000
-            self.log.info(f"{lap_number} = >{bib_number} => {bib} => {time}")
+        for bibNumber in lapResults.keys() : 
+            bib = lapResults[bibNumber]['bibNumber']
+            time = (timeList[bibNumber]['lastLapTime'] - timeList[bibNumber]['startTick'])/1000
+            self.log.info(f"{lapNumber} = >{bibNumber} => {bib} => {time}")
             self.speak_dialog('timing.race', data= {'lap1': 'final', 'bib1':bib, 'time1' : time})
-
 
 def create_skill():
     return RaceTiming()
